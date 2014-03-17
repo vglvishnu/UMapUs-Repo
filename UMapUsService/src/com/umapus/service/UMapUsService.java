1a61c0e820dca6f0b274b4fa8322b947de55276d
vglvishnu <vglvishnu@gmail.com>
2014-03-16 19:43:38 -0700

    Spring

--- UMapUsService/src/com/umapus/service/UMapUsService.java
+++ UMapUsService/src/com/umapus/service/UMapUsService.java
@@ -2,15 +2,18 @@ package com.umapus.service;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
 import org.json.JSONException;
 
 import com.umapus.domain.util.UMapUsMapper;
 import com.umapus.infrastructure.dao.DAOFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -27,11 +30,12 @@ public class UMapUsService {
 	
 
 	@POST
+	@Produces("applciation/json")
 	@Path("/login")
-	public String Login(String jsonBody, @Context HttpServletRequest req) {
+	public String Login(String jsonBody, @Context HttpServletRequest req,@Context HttpServletResponse response) {
 
 		boolean loginStatus = false;
-		String loginResponse = "OUT";
+		String loginResponse = "{\"status\": \"IN\"}";
 		try {
 			loginStatus = daoFactory.getLdapDao().LoginUser(
 					mapper.MapJsonToLoginRequest(jsonBody));
@@ -51,12 +55,13 @@ public class UMapUsService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+       
 		return loginResponse;
 	}
 
 	@POST
 	@Path("/signup")
+	@Produces("applciation/json")
 	public String SignUp(String jsonBody) {
 
 		String gid = "Success";
