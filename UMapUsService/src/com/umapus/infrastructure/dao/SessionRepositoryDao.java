f51d2def5aa244c4ad451516545684ff4bf53f04
vglvishnu <vglvishnu@gmail.com>
2014-03-22 12:50:23 -0700

    Update to SessionRepositoryDao

--- UMapUsService/src/com/umapus/infrastructure/dao/SessionRepositoryDao.java
+++ UMapUsService/src/com/umapus/infrastructure/dao/SessionRepositoryDao.java
@@ -5,4 +5,6 @@ import java.util.HashMap;
 public interface SessionRepositoryDao {
 	public void AddToRedis(String key, HashMap<String,String> hashvalue);
 	public HashMap<String,String> ReadFromRedis(String key);
+	public void UpdateToRedis(String key, HashMap<String,String> hashValue);
+	public void DeleteFromRedis(String key);
 }
--- UMapUsService/src/com/umapus/infrastructure/dao/SessionRepositoryDaoImpl.java
+++ UMapUsService/src/com/umapus/infrastructure/dao/SessionRepositoryDaoImpl.java
@@ -2,6 +2,7 @@ package com.umapus.infrastructure.dao;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,38 +14,45 @@ import com.umapus.domain.entity.UMapUsConstants;
 import com.umapus.domain.util.UMapUsMapper;
 
 public class SessionRepositoryDaoImpl implements SessionRepositoryDao {
-	
+
 	@Autowired
 	private UMapUsMapper umapusmapper;
-	
-	@Autowired
-	private RedisTemplate< String, Object > redisTemplate;
-	
-	private  HashOperations<String, Object, Object> hashOps;
-	
-	
-	public void AddToRedis(String key, HashMap<String,String> hashvalue){
-		
-		redisTemplate.opsForHash().putAll(key, hashvalue);
-		redisTemplate.expire( key, UMapUsConstants.RedisKeyTTLInHrs, TimeUnit.HOURS);
 
-	    //Map<String,String> hm = ReadFromRedis(key);
-		
-//		for (Map.Entry<String, String> entry : hm.entrySet()) {
-//			System.out.println("From Redis Get =" +entry.getKey()+"->"+ entry.getValue());
-//			
-//		}
-		
-		
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+
+	private HashOperations<String, Object, Object> hashOps;
+
+	public void AddToRedis(String key, HashMap<String, String> hashvalue) {
+
+		redisTemplate.opsForHash().putAll(key, hashvalue);
+		redisTemplate.expire(key, UMapUsConstants.RedisKeyTTLInHrs,
+				TimeUnit.HOURS);
+
 	}
-	
-	public HashMap<String,String> ReadFromRedis(String key){
-		
-		
-		BoundHashOperations<String, String, String> userOps = redisTemplate.boundHashOps(key);
-		System.out.println("Redis keys=" + userOps.entries());
-		
+
+	public HashMap<String, String> ReadFromRedis(String key) {
+
+		BoundHashOperations<String, String, String> userOps = redisTemplate
+				.boundHashOps(key);
+		// System.out.println("Redis keys=" + userOps.entries());
+
 		return (HashMap<String, String>) userOps.entries();
 	}
 
+	public void UpdateToRedis(String key, HashMap<String, String> hashValue) {
+		BoundHashOperations<String, String, String> userOps = redisTemplate
+				.boundHashOps(key);
+		Map<String, String> hm = (HashMap<String, String>) userOps.entries();
+		for (Map.Entry<String, String> entry : hashValue.entrySet()) {
+
+			hm.put(entry.getKey(), entry.getValue());
+		}
+		redisTemplate.opsForHash().putAll(key, hm);
+	}
+
+	public void DeleteFromRedis(String key) {
+
+		redisTemplate.delete(key);
+	}
 }
